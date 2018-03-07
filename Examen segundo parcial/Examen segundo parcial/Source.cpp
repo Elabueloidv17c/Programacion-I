b8127b4c47ed32a69a78ee0049a82d1b98185dc6
Elabueloidv17c <34348154+Elabueloidv17c@users.noreply.github.com>
2018-03-06 23:57:43 -0600

    Struct agregado
    hice algunas mejoras en general y agregué el uso de stucts

--- Examen segundo parcial/Examen segundo parcial/Source.cpp
+++ Examen segundo parcial/Examen segundo parcial/Source.cpp
@@ -8,16 +8,23 @@
 
 using namespace std;
 
+struct unidad
+{
+	string m_tipo;
+	string m_valor;
+};
+
 int main()
 {
-	ofstream escritor;
-	escritor.open("oraciones.txt");
 	ifstream lector;
+	ofstream escritor;
 	lector.open("oraciones_examen_practico_parcial2.txt");
+	escritor.open("oraciones.txt");
 
 	srand(time(NULL));
 	vector <string> contenedor;
-	vector <vector<string>> ordenadas;
+	vector <vector<unidad>> ordenadas;
+	string tipos[6] = { "Sujeto", "Adjetivo", "Verbo","Advervio", "Sustantivo","Preposicion" };
 
 	while (!lector.eof())
 	{
@@ -26,37 +33,55 @@ int main()
 		contenedor.push_back(palabra);
 	}
 
-	for (int i = 0; i < 6; i++)
+
+	for (int i = 0; i < contenedor.size(); i++)
 	{
-		vector <string> unidad;
-		ordenadas.push_back(unidad);
+		vector <unidad> palabra;
+		if (i == 0)
+		{
+			ordenadas.push_back(palabra);
+		}
+		if (contenedor[i] == "")
+		{
+			ordenadas.push_back(palabra);
+			contenedor.erase(contenedor.begin() + i);
+		}
 	}
 
 	for (int i = 0; i < ordenadas.size(); i++)
 	{
+		int r = 0;
 		for (int j = 1; j < contenedor.size(); j++)
 		{
-			if (contenedor[j] == "Adjetivo" || contenedor[j] == "Verbo" || contenedor[j] == "Advervio" || 
+			unidad palabra;
+			palabra.m_tipo = tipos[i];
+			palabra.m_valor = contenedor[j];
+			ordenadas[i].push_back(palabra);
+			if (contenedor[j] == "Adjetivo" || contenedor[j] == "Verbo" || contenedor[j] == "Advervio" ||
 				contenedor[j] == "Sustantivo" || contenedor[j] == "Preposicion")
 			{
 				i++;
 				j++;
 			}
-			ordenadas[i].push_back(contenedor[j]);
+	
 		}
 	}
+	
+	cout << "Esto muestra que el tipo de cada palabra es el correcto: " << endl << endl;
 
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < ordenadas.size(); j++)
 		{
 			int aleatorio = rand() % (11 - 8);
-			cout << ordenadas[j][aleatorio] << " ";
-			escritor << ordenadas[j][aleatorio] << " ";
+			cout << ordenadas[j][aleatorio].m_tipo << " ";
+			escritor << ordenadas[j][aleatorio].m_valor << " ";
 		}
-		cout << endl << endl;
+		cout << endl;
 		escritor << endl << endl;
 	}
+	
+	cout << endl << "Las oraciones aleatorias estan guardadas en el archivo 'oraciones.txt'..." << endl;
 
 	cin.get();
 	return 0;
